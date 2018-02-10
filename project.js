cf0316543c48c0fbad88d524078d8a01462997f6
Elena Williams <git@elena.net.au>
2018-02-10 16:10:51 +1100

    pop container on drop
    This is the foundation of the behaviour that we're looking for
    although there are heaps of edge cases that preclude it being
    implemented this way, but this is how it could be done.

--- project.js
+++ project.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
-  dragula([
+
+  var containers = [
     $('#one')[0],
     $('#two')[0],
     $('#tre')[0],
     $('#for')[0] 
-  ])
+  ]
+  var matchUp = dragula(containers)
+
+  matchUp.on('drop', function (el) {
+    var elContainer = $(el).parent()
+    console.log(elContainer);
+    containers.pop(elContainer);
+    matchUp.containers = containers;
+    console.log(matchUp.containers);
+    el.className = el.className.replace('ex-moved', '');
+  })
 });
   
