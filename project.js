c1076fca0c34f5f165502df872cde11f364b761e
Elena Williams <git@elena.net.au>
2018-02-10 18:02:15 +1100

    dragula update parent data with child id

--- project.js
+++ project.js
@@ -2,5 +2,21 @@ $(document).ready(function(){
 
   var containers = $('.container').toArray()
   var matchUp = dragula(containers)
+
+  matchUp.on('drop', function(el, target, source){ 
+    var target = $(target);
+    var source = $(source);
+
+    console.log(source)
+    console.log(target)
+
+    target.data('child-id', $(el).data('child-id'));
+
+    // Remove child from source so not duplicated.
+    source.data('child-id', '');
+
+    console.log(source.data('parent-id') + " => " + source.data('child-id'))
+    console.log(target.data('parent-id') + " => " + target.data('child-id'))
+  })
 });
   
