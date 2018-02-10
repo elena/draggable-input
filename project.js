a1400344f061912bb28a0c3ca617a884219d4606
Elena Williams <git@elena.net.au>
2018-02-10 16:22:38 +1100

    simpler vanilla jquery implementation

--- project.js
+++ project.js
@@ -1,20 +1,6 @@
 $(document).ready(function(){
 
-  var containers = [
-    $('#one')[0],
-    $('#two')[0],
-    $('#tre')[0],
-    $('#for')[0] 
-  ]
+  var containers = $('.container').toArray()
   var matchUp = dragula(containers)
-
-  matchUp.on('drop', function (el) {
-    var elContainer = $(el).parent()
-    console.log(elContainer);
-    containers.pop(elContainer);
-    matchUp.containers = containers;
-    console.log(matchUp.containers);
-    el.className = el.className.replace('ex-moved', '');
-  })
 });
   
