ea7e8f0a406c53243118b5d6d2745a35301bb6c7
Elena Williams <git@elena.net.au>
2018-02-10 13:39:36 +1100

    most basic dragula implementation

--- project.js
+++ project.js
@@ -1,26 +1,3 @@
-var dragAndDrop = {
-  limit: 5,
-  count: 0,
-  init: function() {
-    this.dragula();
-    this.eventListeners();
-  },
-  eventListeners: function() {
-    this.dragula.on('drop', this.dropped.bind(this));
-  },
-  dragula: function() {
-      this.dragula = dragula(
-          [document.querySelector('#left'), document.querySelector('#right')],
-          {moves: this.canMove.bind(this),}
-      );
-
-  },
-  canMove: function() {
-    return this.count < this.limit;
-  },
-  dropped: function(el) {
-    this.count++;
-  }
-};
-
-dragAndDrop.init();
+dragula(
+  [document.querySelector('#left'), document.querySelector('#right')],
+);
